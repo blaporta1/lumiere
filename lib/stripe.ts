ede0a9ad1bb2f99cec0b9017a70e0d84a904005e
blaporta1 <blaporta01@gmail.com>
2026-03-13 22:00:33 -0700

    Add lib/stripe.ts

---
+++ lib/stripe.ts
@@ -0,0 +1,17 @@
+import Stripe from 'stripe';
+
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error('STRIPE_SECRET_KEY is not set in environment variables');
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2024-04-10',
+  typescript: true,
+});
+
+export function formatPrice(amount: number, currency = 'USD'): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency,
+  }).format(amount);
+}
